0a1c68bb4e7352eafc51829a473bbdf995199f48
Delon-Wu <2474323762@qq.com>
2022-12-08 20:31:58 +0800

    [learn] [threejs] learn about mousemove event, OrbitControls

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,23 @@
 import { onMounted, ref } from 'vue';
 import * as THREE from 'three';
 import { Mesh } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+const SIZE = {
+  width: 800,
+  height: 600
+};
 const SCENE = new THREE.Scene();
-const CAMERA = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const CAMERA = new THREE.PerspectiveCamera(60, SIZE.width / SIZE.height, 0.1, 1000);
 const RENDERER = new THREE.WebGL1Renderer();
 const CLOCK = new THREE.Clock();
+let ORBIT_CONSTROL: OrbitControls;
 
 // axes helper
-const AXES_HELPER = new THREE.AxesHelper(100);
-SCENE.add(AXES_HELPER);
+// const AXES_HELPER = new THREE.AxesHelper(100);
+// SCENE.add(AXES_HELPER);
 
-RENDERER.setSize(window.innerWidth, window.innerHeight);
+RENDERER.setSize(SIZE.width, SIZE.height);
 
 // main-page ref
 let mainPage = ref<HTMLDivElement>();
@@ -29,29 +35,25 @@ let mainPage = ref<HTMLDivElement>();
 onMounted(() => {
   mainPage.value?.appendChild(RENDERER.domElement);
   let cube = drawACube();  // 在这之前不会出现任何图形，页面保持空白
-  cube.scale.x = 2;  // 沿x轴放大x倍
 
-  // cube.position.x = .7;
-  // cube.position.y = -.6;
-  // cube.position.z = 1;
-  cube.position.set(2, -.6, 1);
-
-  // Rotation
-  // cube.rotation.x = Math.PI / 4;
-  // cube.rotation.y = Math.PI / 4;
-  // cube.rotation.z = Math.PI / 3;
-  // cube.rotation.reorder('XYZ')
-
-  // Change camera
-  // CAMERA.lookAt(cube.position);
-  // CAMERA.position.x = 1
+  // Control
+  ORBIT_CONSTROL = new OrbitControls(CAMERA, mainPage.value?.childNodes[0] as HTMLElement);
+  ORBIT_CONSTROL.enableDamping = true;
 
   animate(cube)();
-  initMouseEvent(cube);
 });
 
+// const cursorPosition = {
+//   x: 0,
+//   y: 0
+// };
+// window.addEventListener('mousemove', (event: MouseEvent) => {
+//   cursorPosition.x = event.clientX;
+//   cursorPosition.y = event.clientY;
+// })
+
 const drawACube = (): Mesh => {
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const geometry = new THREE.BoxGeometry(1, 1, 1, 5, 5, 5);
   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
   const cubeMesh: Mesh = new THREE.Mesh(geometry, material);
 
@@ -65,19 +67,20 @@ const animate = (cubeMesh: Mesh) => {
     let elapsedTime = CLOCK.getElapsedTime();
     // console.log(elapsedTime);
 
-    // cubeMesh.position.y = Math.sin(elapsedTime);  // the cube flow up like magic
+    // 正方体的正面会跟着你的鼠标改变朝向
+    // cubeMesh.rotation.x = cursorPosition.y / SIZE.height - .5;
+    // cubeMesh.rotation.y = cursorPosition.x / SIZE.width - .5;
+
+    // CAMERA.position.x = -(cursorPosition.x / SIZE.width - .5) * 10;
+    // CAMERA.position.y = (cursorPosition.y / SIZE.height - .5) * 10;
+    // CAMERA.lookAt(new THREE.Vector3());
+
+    ORBIT_CONSTROL.update();
 
     RENDERER.render(SCENE, CAMERA);
 
     requestAnimationFrame(animate(cubeMesh));
   };
-};
-
-const initMouseEvent = (cubeMesh: Mesh) => {
-  document.body.addEventListener('click', (e) => {
-    cubeMesh.rotation.x += .1;
-    cubeMesh.rotation.y += .1;
-  });
 };
 
 </script>
